97503f6098dc92f24346a886ca52955db51d0c83
H91011 <akkaya.hu@gmail.com>
2016-02-10 01:38:37 +0200

    bash scripti

---
+++ docker-oto.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo ""
+sudo docker run -v /home/KULLANICI_ADINIZ/DOSYANIZ/:/git -v /home/KULLANICI_ADINIZ/DOSYANIZ/:/root -v /var/cache/pisi/archives:/var/cache/pisi/archives -v /var/cache/pisi/packages:/var/cache/pisi/packages -itd ertugerata/pisi-chroot-farm bash
+echo "##"
+echo "Dosyalar bağlandı."
+sudo docker ps -a > cikti.txt
+var= $(sed -n '2'p cikti.txt)  "";
+dizi =($var);
+uzunluk=${#dizi};
+echo "Konteynır ismi alındı";
+echo "##";
+konteynir_adi=${dizi[$uzunluk+1]};
+echo ""
+sudo docker start $konteynir_adi
+sudo docker attach $konteynir_adi
+
+"";
+
